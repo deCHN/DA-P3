328385e05884521ff38c624a85514c8389a2ccc0
xnc <sehr.jimmy@gmail.com>
2017-07-21 17:28:59 +0200

    update notebook

--- audit.py
+++ audit.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+import sys
+import codecs
 """
 Your task in this exercise has two steps:
 
-- audit the OSMFILE and change the variable 'mapping' to reflect the changes needed to fix 
+- audit the OSMFILE and change the variable 'mapping' to reflect the changes needed to fix
 the unexpected street types to the appropriate ones in the expected list.
 You have to add mappings only for the actual problems you find in this OSMFILE,
 not a generalized solution, since that may and will depend on the particular area you are auditing.
@@ -14,62 +17,89 @@ from collections import defaultdict
 import re
 import pprint
 
-OSMFILE = "audit_example.osm"
-street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+OSMFILE = "./munich_germany_k10.osm"
 
+street_type_re = re.compile(
+    ur'(\s|-)?(straße|weg|ring|platz|allee|bogen|gasse|brücke|hof|berg|eck)$',
+    re.IGNORECASE | re.UNICODE)
 
-expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road", "Trail", "Parkway", "Commons"]
+expected = [
+    "am",
+    "Am",
+    "an",
+    "An",
+    "Im",
+    "In",
+    "bach",
+    "insel",
+    "kreppe",
+    "park",
+    "hof",
+    "winkel",
+    "garten",
+    "wiese",
+    "wald",
+    "markt",
+    "feld"]
 
 # UPDATE THIS VARIABLE
-mapping = { "St": "Street", "St.": "Street", "Ave": "Avenue", "Rd.": "Road", "St.": "Street" }
+mapping = {
+    "St": "Street",
+    "Str.": "Straße",
+    "Ave": "Avenue",
+    "Rd.": "Road",
+    "St.": "Street"}
 
 
 def audit_street_type(street_types, street_name):
-   m = street_type_re.search(street_name)
-   if m:
-      street_type = m.group()
-      if street_type not in expected:
-         street_types[street_type].add(street_name)
+    m = street_type_re.search(street_name)
+    if m is None and isNotExpected(street_name):
+        street_types[street_name.rsplit(' ', 1)[-1]].add(street_name)
+
+
+def isNotExpected(streetName):
+    for e in expected:
+        if e in streetName:
+            return False
+    return True
+
 
 def is_street_name(elem):
-   return (elem.attrib['k'] == "addr:street")
+    return (elem.attrib['k'] == "addr:street")
 
 
 def audit(osmfile):
-   osm_file = open(osmfile, "r")
-   street_types = defaultdict(set)
-   for event, elem in ET.iterparse(osm_file, events=("start",)):
-      if elem.tag == "node" or elem.tag == "way":
-         for tag in elem.iter("tag"):
-            if is_street_name(tag):
-               audit_street_type(street_types, tag.attrib['v'])
+    osm_file = codecs.open(osmfile, "r", "utf-8")
+    street_types = defaultdict(set)
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
+        if elem.tag == "node" or elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if is_street_name(tag):
+                    audit_street_type(street_types, tag.attrib['v'])
 
-   osm_file.close()
-   return street_types
+    osm_file.close()
+    return street_types
 
 
 def update_name(name, mapping):
-   # YOUR CODE HERE
-   str_type = name.rsplit(' ', 1)
-   if len(str_type) != 2:
-      return name
-   else:
-      return str_type[0] + ' ' +  mapping[str_type[1]]
+    # YOUR CODE HERE
+    str_type = name.rsplit(' ', 1)
+    if len(str_type) != 2:
+        return name
+    else:
+        return str_type[0] + ' ' + mapping[str_type[1]]
+
 
 def test():
-   st_types = audit(OSMFILE)
-   assert len(st_types) == 3
-   pprint.pprint(dict(st_types))
+    st_types = audit(OSMFILE)
+    pprint.pprint(dict(st_types))
 
-   for st_type, ways in st_types.iteritems():
-      for name in ways:
-         better_name = update_name(name, mapping)
-         print name, "=>", better_name
-         if name == "West Lexington St.":
-            assert better_name == "West Lexington Street"
+    # for st_type, ways in st_types.iteritems():
+    # for name in ways:
+    # better_name = update_name(name, mapping)
 
-         if name == "Baldwin Rd.":
-            assert better_name == "Baldwin Road"
 
 if __name__ == '__main__':
-   test()
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    test()
